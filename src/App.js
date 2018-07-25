3f9005ebd1cd1447579e6c848e2b671824ad3124
Isil Donmez <isil.donmez@gmail.com>
2018-07-25 12:21:18 +0100

    Add Analytics to record an appRender event every time a user launches the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import Amplify from 'aws-amplify';
+import Amplify, { Analytics } from 'aws-amplify';
 import awsExports from './aws-exports';
 
 Amplify.configure(awsExports);
 
 class App extends Component {
   render() {
+    Analytics.record('appRender');
     return (
       <Text>Hello World!</Text>
     );
